6af311dfacc18878c165e13eebf2d56da7d28f0d
Yashwandalkar <107640622+Yashwandalkar@users.noreply.github.com>
2022-08-05 20:16:50 +0530

    weather app

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('button')
+const details = document.querySelector('details')
+const name = document.querySelector (".name")
+const desc = document.querySelector("desc")
+const tempC = document.querySelector("temp")
+const iconHolder = document.querySelector('icon')
+const api = '86e5d061b47db077c2b1924672848680'
+
+const input = document.querySelector('#query')
+
+
+btn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    const query = input.value
+   
+})
