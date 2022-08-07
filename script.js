a7c9a0c61f431c7dae864ba462ffe1afa9139857
Yashwandalkar <107640622+Yashwandalkar@users.noreply.github.com>
2022-08-07 14:55:55 +0530

    Weather app

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const btn = document.querySelector('button')
-const details = document.querySelector('details')
-const name = document.querySelector (".name")
-const desc = document.querySelector("desc")
-const tempC = document.querySelector("temp")
-const iconHolder = document.querySelector('icon')
-const api = '86e5d061b47db077c2b1924672848680'
+let weather = {
+    "apikey": "86e5d061b47db077c2b1924672848680",
 
-const input = document.querySelector('#query')
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +
+            "&units=metric&appid="+ this.apikey).then((response) => response.json())
 
+            .then((data) => this.displayWeather(data))
+            .catch((error) => alert("Enter valid address!"))
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".temp").innerHTML = temp + "37&#176;C";
+        document.querySelector(".humidity").innerHTML = "Humidity:" + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind speed:" + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading")
+        // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
-btn.addEventListener('click', (e)=>{
-    e.preventDefault()
-    const query = input.value
-   
-})
+        
+    },
+    
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
+};
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function (e) {
+    if (e.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather()
+// weather.fetchWeather("gujrat")
+
